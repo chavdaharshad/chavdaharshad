20c0f828307a626a9876e04fea4159cc974f107a
Harshad <harshad.chavda@techivies.com>
2022-07-28 17:51:37 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type: 'server', name:'testserver', content:'just a test'}];
-  onAddServer(){
-    // this.serverElements.push({
-    //   type:'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+
+  onServerAdded(serverData: {serverName: string,serverContent: string}){
+    this.serverElements.push({
+      type:'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
   }
-  onAddBlueprint(){
-    // this.serverElements.push({
-    //   type:'blueprint',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+  onBlueprintAdded(blueprintData: {serverName: string,serverContent: string}){
+    this.serverElements.push({
+      type:'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
   }
 
 }
