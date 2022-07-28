0d8112e7374c554bb600a5f765ef8b7dc9fe3a1a
Harshad <harshad.chavda@techivies.com>
2022-07-28 16:46:18 +0530

    First release of Hello World!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practice';
+  serverElements = [{type: 'server', name:'testserver', content:'just a test'}];
+ 
 }
